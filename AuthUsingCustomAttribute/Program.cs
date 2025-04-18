4274ddbca1bc3e980d5ea018d98c31fac47ffc44
alishmsm <alimotlagh45@gmail.com>
2025-04-18 17:32:11 +0330

    change program.cs

--- AuthUsingCustomAttribute/Program.cs
+++ AuthUsingCustomAttribute/Program.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -32,47 +37,16 @@ builder.Services.AddSwaggerGen(options =>
         }
     });
 });
-builder.Services.AddEndpointsApiExplorer(); // برای Endpointهای API
+builder.Services.AddEndpointsApiExplorer();
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger(); // فعال‌سازی میدل‌ور Swagger
-    app.UseSwaggerUI(); // فعال‌سازی UI
+    app.UseSwagger(); 
+    app.UseSwaggerUI(); 
 }
 
 
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
